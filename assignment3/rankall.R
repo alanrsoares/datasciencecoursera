c68562a97cff3200ff7cc615a31d4fabaaa4a6f5
Alan R. Soares <alanrsoars@gmail.com>
2015-05-05 01:44:49 +1200

    refactors for better reusability

--- assignment3/rankall.R
+++ assignment3/rankall.R
@@ -1,14 +1,9 @@
 source('rankhospital.R')
 
 rankall <- function(outcome, num = 'best') {
-  data <- read.csv('data/outcome-of-care-measures.csv', colClasses = 'character')
-  states <- sort(unique(data[, 7]))
-  column <- switch( outcome
-                  , 'heart attack'  = 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack'
-                  , 'heart failure' = 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure'
-                  , 'pneumonia'     = 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia'
-                  , stop('invalid outcome')
-                  )
+
+  states <- get_states()
+  column <- get_outcome_column(outcome)
   
   rank_hospital_by_state <-  function(state) rankhospital(state, outcome, num) 
   
--- assignment3/rankhospital.R
+++ assignment3/rankhospital.R
@@ -1,16 +1,28 @@
+
+get_outcome_column <- function(outcome){
+  switch( outcome  
+        , 'heart attack'  = 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack'
+        , 'heart failure' = 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure'
+        , 'pneumonia'     = 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia'
+        , stop('invalid outcome')
+        )
+}
+
+read_measures <- function() read.csv('data/outcome-of-care-measures.csv', colClasses = 'character')
+  
+get_states <- function(){
+  data <- read_measures()
+  sort(unique(data[, 7]))
+}
+
 rankhospital <- function(state, outcome, num = 'best') {
-  data <- read.csv('data/outcome-of-care-measures.csv', colClasses = 'character')
-  states <- unique(data[, 7])
+  data <- read_measures()
   
   # validate state
-  if(!is.element(state, states))
+  if(!is.element(state, get_states()))
     stop('invalid state')
 
-  column <- switch(outcome
-                   , 'heart attack'  = 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack'
-                   , 'heart failure' = 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure'
-                   , 'pneumonia'     = 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia'
-                   , stop('invalid outcome'))
+  column <- get_outcome_column(outcome)
   
   # data filtered by state
   data <- data[data$State == state, c("Hospital.Name", column)]
